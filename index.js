e034f4880f6bab1e53208f721dec7deef48f8ff1
kh-almas <services.almas@gmail.com>
2023-05-03 14:37:44 +0600

    make recips json file and set url for get data

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const cors = require('cors');
 
 
 const chef = require('./data/chef.json');
+const items = require('./data/recipes.json');
 
 app.use(cors())
 
 app.get('/', (request, response) => {
-    response.send('news is on the way');
+    response.send('the_plate_escape is on the way');
 });
 app.get('/chef', (request, response) => {
     response.send(chef);
 });
+app.get('/chef/item/:id', (request, response) => {
+    const id = request.params.id;
+    const finaldata = items.all_recipes.find(n => id === n.chef_id);
+    console.log(finaldata);
+});
 
 app.listen(port, () => {
-    console.log(`dragonNews app listening on port ${port}`)
+    console.log(`the_plate_escape app listening on port ${port}`)
 });
