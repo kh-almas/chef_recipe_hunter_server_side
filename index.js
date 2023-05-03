35237fcb19656307640f2fb763ff90cd7b43a4a3
kh-almas <services.almas@gmail.com>
2023-05-03 10:33:30 +0600

    route setup

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
+// const port = process.ENV.PORT || 5000;
 const port = 5000;
 
+const cors = require('cors');
 
+
+
+const chef = require('./data/chef.json');
+
+app.use(cors())
 
 app.get('/', (request, response) => {
     response.send('news is on the way');
 });
+app.get('/chef', (request, response) => {
+    response.send(chef);
+});
 
 app.listen(port, () => {
-    console.log(`the_plate_escape app listening on port ${port}`)
+    console.log(`dragonNews app listening on port ${port}`)
 });
